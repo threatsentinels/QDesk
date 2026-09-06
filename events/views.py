5e4b8526619fa87087538761a8e83ac191e1ee0d
threatsentinels <threatsentinels@gmail.com>
2026-09-06 17:51:05 +0000

    wrote views for frotnend functions

--- events/views.py
+++ events/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect , get_object_or_404
+from organizations.models import Organization 
 
+from .models import Event
+
+from .forms import EventForm
 # Create your views here.
+
+
+def create_event(request,organization_id):
+    organization = get_object_or_404(
+        Organization,
+        id=organization_id
+    )
+
+    if request.method =="POST":
+
+        form = EventForm(request.POST)
+
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.organization = organization
+            event.save()
+
+            return redirect(
+                "events:detail",
+                event_id=event.id
+            )
+        else:
+            form = EventForm()
+
+        return render(
+            request,
+            "events/create.html",
+            {
+                "form":form,
+                "organization":organization,
+            }
+
+        )
+
+def event_detail(request,event_id):
+    event  = get_object_or_404(Event , id = event_id)
+
+    return render(
+        request,
+        "events/detail.html",
+        {"event":event}
+
+    )
--- organizations/views.py
+++ organizations/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect 
+from .forms import OrganizationForm 
+
 
 # Create your views here.
+
+
+def create_organization(request):
+    if request.method =="POST":
+        form = OrganizationForm(request.POST,request.FILES)
+
+        if form.is_valid():
+            organization = form.save()
+            return redirect(
+                "events:create",
+                organization_id=organization.id
+
+            )
+        else:
+            form = OrganizationForm()
+            return render(
+                request,
+                "organizations/create.html",
+                {"form":form}
+                
+            )
