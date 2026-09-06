1d78f122f22dc82f512c864b369b939541e4981a
threatsentinels <threatsentinels@gmail.com>
2026-09-06 17:33:22 +0000

    form for events app

---
+++ events/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Event 
+
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model=Event
+        fields = [
+            "name",
+            "location",
+            "token_prefix",
+            "starting_number",
+        ]
